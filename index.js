ffabe0339640a52585084950915b3b66594f2553
Aditishrivas <124878443+Aditishrivas@users.noreply.github.com>
2023-04-28 22:53:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const userInput = document.getElementById("userInput");
+var expresion = '';
+
+function press(num) {
+    expresion += num;
+    userInput.value = expresion;
+}
+
+function equal() {
+    userInput.value = eval(expresion)
+    expresion = '';
+}
+
+function erase() {
+    expresion = '';
+    userInput.value = expresion
+}
